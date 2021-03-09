41517a0099d2494da0a80116d4e57c9873b51d48
tranthuhien0711 <80303687+tranthuhien0711@users.noreply.github.com>
2021-03-09 11:46:01 +0700

    Add files via upload

---
+++ KhachHangController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLBH.Models;
+
+namespace QLBH.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        //khai báo đói tượng kết nối tới database
+        QLBHDbContext db = new QLBHDbContext();
+        // GET: KhachHang
+        public ActionResult Index()
+        {
+            //lấy toàn bộ dl trong bảng khách hàng 
+            //trả về dạng list ròi hiển thị lên view
+            return View(db.KhachHangs.ToList());
+        }
+        //tạo action create trả về view cho người dùng
+        //nhap thông tin khách hàng đê thêm mới vời csdl
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //tạo action create với tham số để nhận dl từ client
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(KhachHang kh)
+        {
+            //nếu đk ràng buộc ở class được thỏa mãn
+            if (ModelState.IsValid)
+            {
+                //thêm moi đói tượng kh vào trong csdl
+                db.KhachHangs.Add(kh);
+                //cập nhật thay đổi vào csdl
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }    
+            //nếu đk ràng buộc ở class k thỏa mãn 
+            return View(kh);
+        }
+    }
+
+}
---
+++ NhanVienController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLBH.Models;
+
+namespace QLBH.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        //khai báo đối tượng kết nối tới database
+        QLBHDbContext db = new QLBHDbContext();
+        // GET: NhanVien
+        public ActionResult Index()
+        {
+            //lấy toàn bộ dữ liệu trong bảng nhanvien
+            //trả về dạng list rồi hiển thị len View
+            return View(db.NhanViens.ToList());
+        }
+        //tạo action create trả về view cho người dùng
+        //nhập thông tin khách hàng để thêm mới vào CSDL
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //tạo action create với tham số để nhận dl từ client
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult create(NhanVien nv )
+        {
+            //nếu đk ràng buộc ở Class được thỏa mãn
+            if (ModelState.IsValid)
+            {
+                //thêm mới đối tượng nv vào CSDl
+                db.NhanViens.Add(nv);
+                //cập nhật thay đổi vào CSDL
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+    }
+}
